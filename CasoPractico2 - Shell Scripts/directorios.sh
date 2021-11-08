66524c27c6d25f03ba5f6da2879864f7d6f1ca5e
Javier Aspiroz <javiaspiroz@gmail.com>
2021-11-08 22:29:36 +0100

    añado comentario aclaratorio

--- CasoPractico2 - Shell Scripts/directorios.sh
+++ CasoPractico2 - Shell Scripts/directorios.sh
@@ -7,7 +7,7 @@ for i in A B C
         cd D$i
         for j in 1 2 3
             do
-                touch f${i,,}$j
+                touch f${i,,}$j #las dos comas se usan para pasar la i a minuscula
             done
         cd ..
     done
